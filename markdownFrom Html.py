4339cdb59d4988e6c34dca2934eae737ea8b2473
chappers <chapm0n.siu@gmail.com>
2012-12-03 07:53:18 +1100

    Update markdownFrom Html.py

--- markdownFrom Html.py
+++ markdownFrom Html.py
@@ -3,19 +3,18 @@ import markdown
 import os
 
 # Capture our current directory
-##THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-THIS_DIR = 'C:/Users/CHAPMAN/Desktop/ToC/'
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Create the jinja2 environment.
 # Notice the use of trim_blocks, which greatly helps control whitespace.
 j2_env = Environment(loader=FileSystemLoader(THIS_DIR))
 
-inputFile = open('C:\Users\CHAPMAN\Desktop\ToC\markdownText.txt','r').read()
+inputFile = open('markdownText.txt','r').read()
 html = markdown.markdown(inputFile);
 
 htmlFile = j2_env.get_template('ToCtemplate.html').render(mdDocument=html)
 
-outputFile = open('C:\Users\CHAPMAN\Desktop\ToC\markdownText.html','w')
+outputFile = open('markdownText.html','w')
 outputFile.write(htmlFile)
 outputFile.close()
 
